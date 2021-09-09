b35f6b59e7dc3689f690d8b5aac013dad455a2d4
Caroline Rolim <ckbrolim@gmail.com>
2021-09-09 17:15:57 -0300

    Cria componentes

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './componentes/calculadora.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculadoraComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    CalculadoraComponent
   ]
 })
 export class CalculadoraModule { }
