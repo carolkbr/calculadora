51585febb23b2995e7c39505c53a08d1c69d1402
Caroline Rolim <ckbrolim@gmail.com>
2021-09-09 19:54:11 -0300

    Adiciona teste unitário e finaliza projeto

--- src/app/calculadora/componentes/calculadora.component.spec.ts
+++ src/app/calculadora/componentes/calculadora.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { CalculadoraComponent } from './calculadora.component';
+import { CalculadoraService } from '../services/calculadora.service';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -22,4 +23,26 @@ describe('CalculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  }); 
 });
